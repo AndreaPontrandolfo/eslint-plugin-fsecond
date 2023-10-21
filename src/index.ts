3b597d308aee22c4e9f73745a978439b3af205bb
AndreaPontrandolfo <andrea.pontra@gmail.com>
2023-10-22 00:05:17 +0200

    fix: corrected entry point

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,15 @@
 import preferDestructuredOptionals from "./rules/prefer-destructured-optionals";
 import ensureMatchingRemoveEventListener from "./rules/ensure-matching-remove-event-listener";
 
-export default {
+const plugin = {
+  meta: {
+    name: "eslint-plugin-fsecond",
+    version: "1.1.0",
+  },
   rules: {
     "prefer-destructured-optionals": preferDestructuredOptionals,
     "ensure-matching-remove-event-listener": ensureMatchingRemoveEventListener,
   },
 };
+
+export default plugin;
